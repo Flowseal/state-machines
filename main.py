9532d3f4682caaa569c5d1004f220f83b6ef4ac0
Flowseal <sellyalegium@gmail.com>
2024-09-30 01:02:05 +0300

    completed mealy to moore

--- main.py
+++ main.py
@@ -10,50 +10,75 @@ class Modes(str, enum.Enum):
 
 def mealy_to_moore(input_file, output_file):
     with open(input_file) as f:
-        input_data = f.read()
+        input_data = f.read().strip()
         
-    replacements: dict[str, str] = dict()
-    state_outputs: dict[str, set[str]] = defaultdict(set)
+    replacements: dict[str, str] = dict()  # example: "F1/0" -> "R0", "F1/1" -> "R1" 
+    states: list[str] = list()  # literally splitted first line of mealy input file
+    state_outputs: dict[str, set[str]] = defaultdict(set)  # state -> set of produced output symbols
+    input_to_transitions: dict[str, dict[str, str]] = defaultdict(dict)  # symbol -> current state -> next state/output
 
-    # Save input symbols and state/outputs pairs
+    # Save states
+    for state in input_data.splitlines()[0].split(";")[1:]:
+        states.append(state.strip())
+
+    # Save transitions
     for transitions in input_data.splitlines()[1:]:
-        for transition in transitions.split(";")[1:]:
-            state, output = transition.split("/")   
-            state_outputs[state].add(output)
+        input_symbol = transitions.split(";")[0].strip()
 
+        for i, transition in enumerate(transitions.split(";")[1:]):
+            state, output = transition.strip().split("/")
+            state_outputs[state].add(output)
+            input_to_transitions[input_symbol][states[i]] = f"{state}/{output}"
+
+    # Sort transitions
     state_outputs = dict(sorted(state_outputs.items()))
     for state in state_outputs:
-        state_outputs[state] = sorted(state_outputs[state])   
-    
-    # Create replacement for states without transitions in themselves
-    for state in input_data.splitlines()[0].split(";")[1:]:
-        if state not in state_outputs:
-            replacements[state] = "R" + str(len(replacements))
+        state_outputs[state] = sorted(state_outputs[state])
 
-    # Create replacement for transitions
-    for state, outputs in state_outputs.items():
-        for output in outputs:
-            transition = f"{state}/{output}"
-            if transition not in replacements:
+    # Create replacement for states and transitions
+    for state in states:
+        if state not in state_outputs: 
+            replacements[state] = "R" + str(len(replacements))
+        else:
+            for output in state_outputs[state]:
+                transition = f"{state}/{output}"
                 replacements[transition] = "R" + str(len(replacements))
 
     # Convert to moore
     outputs_line = [""]
     states_line = [""]
+    input_lines = []
 
+    # Outputs and states lines
+    for state in states:
+        if state not in state_outputs:
+            outputs_line.append("")
+            states_line.append(replacements[state])
+        else:
+            for output in state_outputs[state]:
+                outputs_line.append(output)
+                states_line.append(replacements[f"{state}/{output}"])
 
-# Mealy
-#   ; F1 ; F2 ; F3 
-# b1;F1/0;F1/1;F1/2 
-# b2;F2/1;F2/0;F2/1
-# b3;F3/2;F3/1;F3/0
+    # Input lines
+    for symbol in input_to_transitions:
+        line = []
+        line.append(symbol)
 
-# Moore
-#   ;0 ;1 ;2 ;0 ;1 ;0 ;1 ;2 
-#   ;R0;R1;R2;R3;R4;R5;R6;R7
-# b1;R0;R0;R0;R1;R1;R2;R2;R2  
-# b2;R4;R4;R4;R3;R3;R4;R4;R4  
-# b3;R7;R7;R7;R6;R6;R5;R5;R5
+        for current_state in input_to_transitions[symbol]:
+            next_state = input_to_transitions[symbol][current_state]
+            repeats = 1 if current_state not in state_outputs else len(state_outputs[current_state])
+
+            for _ in range(repeats):
+                line.append(replacements[next_state])
+        
+        input_lines.append(line)
+    
+    # Write to output
+    with open(output_file, "w") as f:
+        f.write(";".join(outputs_line) + "\n")
+        f.write(";".join(states_line))
+        for line in input_lines:
+            f.write("\n" + ";".join(line))
 
 
 def moore_to_mealy(input_file, output_file):
